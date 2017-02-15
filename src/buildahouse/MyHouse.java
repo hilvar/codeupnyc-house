36568eeefdc3ff5de1c1773d0fb81953645053cd
Andy Jennings <andyj@shazbot.net>
2017-02-15 09:48:53 -0500

    Setup files for work and add IntelliJ files

--- src/buildahouse/MyHouse.java
+++ src/buildahouse/MyHouse.java
@@ -11,11 +11,9 @@ public class MyHouse {
   public static void build_me() {
     // Declare width
     // Set width to the number of blocks
-    int width = 8;
 
     // Declare height
     // Set height to the number of blocks
-    int height = 4;
 
     BuildAHouse.buildMyHouse(width, height);
   }
